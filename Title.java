f9b0141e7d81489e526e92dd200c1116c658a04d
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-06-15 08:38:49 -0400

    Scuffed but working game

--- Title.java
+++ Title.java
@@ -1,27 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GameOver here.
+ * Write a description of class Title here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class GameOver extends World
+public class Title extends World
 {
     GreenfootImage blank = new GreenfootImage("Wall.jpg");
-    Label gameOver = new Label("Game Over", 50);
-    Label score = new Label("Score: " + Pellet.points, 50);
+    Label gameName = new Label("Pacman 2.0", 50);
+    Label instructions = new Label("Press Enter to Start", 50);
     /**
      * Constructor for objects of class GameOver.
      * 
      */
-    public GameOver()
+    public Title()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(30, 25, 20); 
         blank.scale(600, 500);
         setBackground(blank);
-        addObject(gameOver, 15, 11);
-        addObject(score, 15, 13);
+        addObject(gameName, 15, 11);
+        addObject(instructions, 15, 13);
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("enter")){
+            Greenfoot.setWorld(new Background());
+        }
     }
 }
