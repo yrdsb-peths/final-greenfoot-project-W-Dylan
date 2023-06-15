817822fab4e9ec5b9ede9c003bf8419cc04c6f59
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-06-15 08:22:42 -0400

    Added Point system

--- Pellet.java
+++ Pellet.java
@@ -8,11 +8,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Pellet extends Actor
 {
+    public static int points = 0;
     /**
      * Act - do whatever the Pellet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+    }
+    
+    public void addPoint(){
+        points++;
+    }
+    
+    public int givePoint(){
+        return points;
     }
 }
