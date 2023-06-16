73701005c1eadbb64336b04c6ea5fc040d6f8c6b
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-06-15 23:21:08 -0400

    Comment update

--- Shop.java
+++ Shop.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Shop here.
  * 
- * @author (your name) 
+ * @author (Dylan Windsor) 
  * @version (a version number or a date)
  */
 public class Shop extends World
