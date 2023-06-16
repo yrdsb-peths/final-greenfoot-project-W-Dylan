021534e63c318e2b5e9bd24aee307fba507e0cca
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-06-15 23:24:44 -0400

    Title addition

---
+++ Title.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends World
+{
+    GreenfootImage blank = new GreenfootImage("Wall.jpg");
+    Label gameName = new Label("Pacman 2.0", 50);
+    Label instructions = new Label("Press Enter to Start", 50);
+    /**
+     * Constructor for objects of class GameOver.
+     * 
+     */
+    public Title()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(30, 25, 20); 
+        blank.scale(600, 500);
+        setBackground(blank);
+        addObject(gameName, 15, 11);
+        addObject(instructions, 15, 13);
+        
+        Ghost.spawnRate = 3000;
+        Ghost.maxGhost = 20;
+        Ghost.speed = 100;
+        Pellet.points = 0;
+        Pellet.pelletPoint = 1;
+        Pacman.movementRate = 500;
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("enter")){
+            Greenfoot.setWorld(new Background());
+        }
+    }
+}
